4a85a6f48ba784899eaeeedcfcaffa352960afd6
joseph chaudhari <chaudharijoe@gmail.com>
2021-01-20 13:30:11 -0600

    9th commmit

--- hw4.js
+++ hw4.js
@@ -1,65 +1,96 @@
-// create 15 second timer 
-// function startTimer(duration, display) {
-//     var timer = duration, minutes, seconds;
-//     setInterval(function () {
-//         minutes = parseInt(timer / 60, 10);
-//         seconds = parseInt(timer % 60, 10);
-
-//         minutes = minutes < 10 ? "0" + minutes : minutes;
-//         seconds = seconds < 10 ? "0" + seconds : seconds;
-
-//         display.textContent = minutes + ":" + seconds;
-
-//         if (--timer < 0) {
-//             timer = duration;
-//         }
-//     }, 1500);
-//         if (--timer == 0){
-//             return('Sorry - Out of Time');
-//             clearInterval(timer);
-//         }
-// }
-// window.onload = function () {
-//     var oneMinute = 15 * 1,
-//         display = document.querySelector('#time');
-//     startTimer(oneMinute, display);
-// };
-
-window.onload = function(){
-(function(){
-  var counter = 20;
-  setInterval(function() {
-    counter--;
-    if (counter >= 0) {
-      span = document.getElementById("time");
-      span.innerHTML = counter;
-    }
-    if (counter === 0) {
-        alert('Test Over');
-        clearInterval(counter);
-    }
-  }, 2000);
-})();
+window.onload = function () {
+    (function () {
+        var counter = 20;
+        setInterval(function () {
+            counter--;
+            if (counter >= 0) {
+                span = document.getElementById("time");
+                span.innerHTML = counter;
+            }
+            if (counter === 0) {
+                alert('Test Over');
+                clearInterval(counter);
+            }
+        }, 1000);
+    })();
 };
 // Question 1 interactive answer
-var submitAnswer1 = function() {
+var submitAnswer1 = function () {
 
     var radios = document.getElementsByName('choice');
-    var val= "";
+    var val = "";
     for (var i = 0, length = radios.length; i < length; i++) {
         if (radios[i].checked) {
-           val = radios[i].value; 
-           break;
-         }
+            val = radios[i].value;
+            break;
+        }
     }
-    
-    if (val == "" ) {
-      alert('please select choice answer');
-    } else if ( val == "Lycurgus" ) {
-      alert('Answer is correct !');
-    } else {
-      alert('Answer is wrong');
-    }
-  };
 
+    if (val == "") {
+        alert('please select choice answer');
+    } else if (val == "Lycurgus") {
+        alert('Answer is correct !');
+        $("#submitAnswer1").click(function() {
+            $("#questions1").fadeOut("slow", function(){})
+        });
+        $("#submitAnswer1").click(function() {
+            $("#questions2").fadeIn("slow", function(){})
+        });
+    } else {
+        alert('Answer is wrong');
+    }
+};
+// Q2 Interactive answer selection
+var submitAnswer2 = function () {
+
+    var radios = document.getElementsByName('choice');
+    var val = "";
+    for (var i = 0, length = radios.length; i < length; i++) {
+        if (radios[i].checked) {
+            val = radios[i].value;
+            break;
+        }
+    }
+
+    if (val == "") {
+        alert('please select choice answer');
+    } else if (val == "Byzantium") {
+        alert('Answer is correct !');
+        $("#submitAnswer2").click(function() {
+            $("#questions2").fadeOut("slow", function(){})
+        });
+        $("#submitAnswer2").click(function() {
+            $("#questions3").fadeIn("slow", function(){})
+        });
+    } else {
+        alert('Answer is wrong');
+    }
+};
+
+// Q3 Interactive answer selection
+var submitAnswer3 = function () {
+
+    var radios = document.getElementsByName('choice');
+    var val = "";
+    for (var i = 0, length = radios.length; i < length; i++) {
+        if (radios[i].checked) {
+            val = radios[i].value;
+            break;
+        }
+    }
+
+    if (val == "") {
+        alert('please select choice answer');
+    } else if (val == "Argyraspides") {
+        alert('Answer is correct !');
+        $("#submitAnswer3").click(function() {
+            $("#questions3").fadeOut("slow", function(){})
+        });
+        // $("#submitAnswer3").click(function() {
+        //     $("#questions2").fadeIn("slow", function(){})
+        // });
+    } else {
+        alert('Answer is wrong');
+    }
+};
 
